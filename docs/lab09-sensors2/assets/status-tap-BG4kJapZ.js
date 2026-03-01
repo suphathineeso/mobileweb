95f1668038914edca5c51a6db21eea91be3e0532
suphathineeso <suphathinee.s@kkumail.com>
2026-03-01 21:04:36 +0700

    fix gh-pages base href and redeploy lab09

---
+++ docs/lab09-sensors2/assets/status-tap-BG4kJapZ.js
@@ -0,0 +1,3 @@
+import{F as a,G as i,H as c,J as d,K as l}from"./index-D_4VS2rB.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
--- docs/lab09-sensors2/assets/web-BJu7l8wI.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./Tab1Page-DJEOmHDd.js";import"./index-C7q3mDg_.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";class n extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{n as MotionWeb};
